0d9321e40ec2d578eb3d44b1c071edd14c0b97aa
xinranwang111 <88405078+xinranwang111@users.noreply.github.com>
2022-07-26 00:58:22 -0700

    Updated

--- example.slurm.sh
+++ example.slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --time=01:00:00
-#SBATCH --mem=8Gb
-#SBATCH --array=1-4000
+#SBATCH --time=10:00:00
+#SBATCH --mem=32Gb
+#SBATCH --array=1-2000
 
 if [ ! $SLURM_ARRAY_TASK_ID ]; then
   NR=$1
@@ -10,20 +10,93 @@ else
   NR=$SLURM_ARRAY_TASK_ID
 fi
 
+eval "$(conda shell.bash hook)"
+conda activate twas_sim
 source ~/init.sh
 
 start=`python -c "print( 1 + 10 *int(int($NR-1)))"`
 stop=$((start + 9))
 
+# gene complexity of sampled region
+lower=5
+upper=20
+
+hapmap=/project/nmancuso_8/xwang505/twas_sim/HAPMAP_SNPS/
+loci=/project/nmancuso_8/xwang505/twas_sim/ind_loci.bed
+genes=/project/nmancuso_8/xwang505/twas_sim/glist-hg19.nodupe.autosome
+
+# point to plink installation
+#plink=~/bin/plink2/plink2
+plink=/project/nmancuso_8/xwang505/tools/plink2
+
+# point to reference panel dir
+okg=/project/nmancuso_8/data/LDSC/1000G_EUR_Phase3_plink/
+
+tdir=/scratch1/xwang505/tmp/
+
+# change to point to results/output directory
+odir=/scratch1/xwang505/TWAS/genotype
+
+# minimum number of SNPs that need to exist for simulation
+MIN_SNPS=450
+
+# get genotype
+for IDX in `seq $start $stop`
+do
+  while [[ ! -e $odir/twas_sim${SIM}_loci${locus}.bim &&  `wc -l $odir/twas_sim${SIM}_loci${locus}.bim | awk '{print $1}'` -gt $MIN_SNPS ]]
+  do
+    echo "attempting ${IDX}"
+    python /project/nmancuso_8/xwang505/twas_sim/sample_genes.py \
+    $loci \
+    $genes \
+    -l $lower \
+    -u $upper \
+    -o $tdir/gene_twas_sim_loci${IDX}.list \
+    --loc_output $tdir/locus_twas_loci${IDX}.txt \
+    --seed $IDX
+
+    params=`sed "1q;d" $tdir/locus_twas_loci${IDX}.txt`
+    set -- junk $params
+    shift
+
+    #1 	 10583 	 1892607
+    # get the region bounds
+    chr=$1
+    numchr=`echo $chr | sed 's/chr//'`
+    locus_start=$2
+    locus_stop=$3
+
+    # replace with your path to PLINK reference data
+    $plink --bfile $okg/1000G.EUR.QC.${numchr} \
+    --chr $numchr \
+    --from-bp $locus_start \
+    --to-bp $locus_stop \
+    --make-bed \
+    --out $odir/twas_sim_loci${IDX} \
+    --snps-only \
+    --hwe midp 1e-5 \
+    --geno 0.01 \
+    --maf 0.01 \
+    --allow-no-sex \
+    --memory 2048 \
+    --keep /project/nmancuso_8/xwang505/twas_sim/EUR.samples \
+    --extract $hapmap/hm.$numchr.snp \
+    --force-intersect
+
+  done
+
+done
+
+# run simulation
 for IDX in `seq $start $stop`
 do
   # PARAMETERS
-  params=`sed "$((IDX+1))q;d" /scratch1/xwang505/TWAS/twas_sim/param_twas_sim.tsv`
+  params=`sed "$((IDX+1))q;d" /project/nmancuso_8/xwang505/twas_sim/param_twas_sim.tsv`
   echo "$((IDX+1)) ${params}"
   set -- junk $params
   shift
 
-  # SIM	ID	N	NGE	MODEL	H2G	H2GE	fastGWAS	LINEAR_MODEL
+  # SIM	ID	N	NGE	MODEL	H2G	H2GE	LINEAR_MODEL
   SIM=$1
   locus=$2
   N=$3 # N GWAS
@@ -31,27 +104,39 @@ do
   MODEL=$5 # eQTL model; see sim.py for details
   H2G=$6 # eQTL h2g
   H2GE=$7 # variance explained in complex trait; 0 (null) to 0.01 (huge effect) are reasonable values
-  fastGWAS=$8
-  LINEAR_MODEL=$9
+  LINEAR_MODEL=$8
 
-
-  OUT=/scratch1/xwang505/TWAS/res_check/twas_sim${SIM}_loci${locus}
+  OUT=/scratch1/xwang505/TWAS/res/twas_sim${SIM}_loci${locus}
   rm -rf $OUT*
-  oloci=/project/nmancuso_8/xwang505/genotype/twas_sim_loci${locus}
+  oloci=/scratch1/xwang505/TWAS/genotype/twas_sim_loci${locus}
 
-  echo "running simulation"
-  python /scratch1/xwang505/TWAS/twas_sim/sim.py \
+  echo "running fast simulation"
+  python /project/nmancuso_8/xwang505/twas_sim/sim.py \
   $oloci \
   --ngwas $N \
   --nqtl $NGE \
-  --model $MODEL \
+  --ncausal $MODEL \
   --eqtl-h2 $H2G \
+  --fast-gwas-sim \
   --var-explained $H2GE \
-  --fast-gwas-sim $fastGWAS \
   --linear-model $LINEAR_MODEL \
   --seed ${locus} \
   --locus ${locus} \
   --sim ${SIM} \
-  --output $OUT
+  --output $OUT.fast
+
+  echo "running std simulation"
+  python /project/nmancuso_8/xwang505/twas_sim/sim.py \
+  $oloci \
+  --ngwas $N \
+  --nqtl $NGE \
+  --ncausal $MODEL \
+  --eqtl-h2 $H2G \
+  --var-explained $H2GE \
+  --linear-model $LINEAR_MODEL \
+  --seed ${locus} \
+  --locus ${locus} \
+  --sim ${SIM} \
+  --output $OUT.std
 
 done
