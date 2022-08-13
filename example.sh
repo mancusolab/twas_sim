a45eab3c051922619b0d04f75bc92156c6af1891
xinranwang111 <88405078+xinranwang111@users.noreply.github.com>
2022-08-13 14:07:39 -0700

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 if [ $1 ]; then
-    locus=$1
+    IDX=$1
 else
-    locus=1
+    IDX=1
 fi
-
-eval "$(conda shell.bash hook)"
-conda activate twas_sim
-source ~/init.sh
 
 # gene complexity of sampled region
 lower=5
 upper=20
 
-hapmap=/project/nmancuso_8/xwang505/twas_sim/HAPMAP_SNPS/
-loci=/project/nmancuso_8/xwang505/twas_sim/ind_loci.bed
-genes=/project/nmancuso_8/xwang505/twas_sim/glist-hg19.nodupe.autosome
+hapmap=/scratch1/xwang505/TWAS/twas_sim/HAPMAP_SNPS/
+loci=/scratch1/xwang505/TWAS/twas_sim/ind_loci.bed
+genes=/scratch1/xwang505/TWAS/twas_sim/glist-hg19.nodupe.autosome
 
 # point to plink installation
 plink=/project/nmancuso_8/xwang505/tools/plink2
@@ -27,9 +23,7 @@ okg=/project/nmancuso_8/data/LDSC/1000G_EUR_Phase3_plink/
 # PARAMETERS
 # change to point to results/output directory
 odir=/scratch1/xwang505/TWAS/genotype_check
-
-# minimum number of SNPs that need to exist for simulation
-MIN_SNPS=450
+rdir=/scratch1/xwang505/TWAS/res_check
 
 N=100000 # N GWAS
 NGE=500 # N EQTL
@@ -39,12 +33,12 @@ H2GE=0.001 # variance explained in complex trait; 0 (null) to 0.01 (huge effect)
 LINEAR_MODEL=trueqtl
 
 # get genotype
-while [ ! -e $odir/twas_sim${locus}.bim ]
+while [[ ! -e $odir/twas_sim_loci${IDX}.bim ]]
 do
-    echo "attempting ${locus}"
-    python sample_genes.py $loci $genes -l $lower -u $upper -o $odir/gene.${locus}.list --loc_output $odir/locus.${locus}.txt
+    echo "attempting ${IDX}"
+    python sample_genes.py $loci $genes -l $lower -u $upper -o $odir/gene.${IDX}.list --loc_output $odir/locus.${IDX}.txt
 
-    params=`sed "1q;d" $odir/locus.${locus}.txt`
+    params=`sed "1q;d" $odir/locus.${IDX}.txt`
     set -- junk $params
     shift
 
@@ -55,34 +49,33 @@ do
     locus_start=$2
     locus_stop=$3
 
-
     # replace with your path to PLINK reference data
     $plink --bfile $okg/1000G.EUR.QC.${numchr} \
     --chr $numchr \
     --from-bp $locus_start \
     --to-bp $locus_stop \
     --make-bed \
-    --out $odir/twas_sim${locus} \
+    --out $odir/twas_sim${SIM}_loci${IDX} \
     --snps-only \
     --hwe midp 1e-5 \
     --geno 0.01 \
     --maf 0.01 \
     --allow-no-sex \
     --memory 2048 \
-    --keep /project/nmancuso_8/xwang505/twas_sim/EUR.samples \
+    --keep /scratch1/xwang505/TWAS/twas_sim/EUR.samples \
     --extract $hapmap/hm.$numchr.snp \
     --force-intersect
 
+    if [[ `wc -l $odir/twas_sim_loci${IDX}.bim | awk '{print $1}'` -lt $MIN_SNPS ]];
+    then
+      rm $odir/twas_sim_loci${IDX}.{bim,bed,fam}
+    fi
 done
-
-OUT=/scratch1/xwang505/TWAS/res_check/twas_sim${locus}
-rm -rf $OUT*
-oloci=/scratch1/xwang505/TWAS/genotype_check/twas_sim${locus}
 
 # run simulation
 echo "running fast simulation"
 python /project/nmancuso_8/xwang505/twas_sim/sim.py \
-$oloci \
+${odir}/twas_sim_loci${IDX} \
 --ngwas $N \
 --nqtl $NGE \
 --ncausal $MODEL \
@@ -90,15 +83,18 @@ $oloci \
 --fast-gwas-sim \
 --var-explained $H2GE \
 --linear-model $LINEAR_MODEL \
---output $OUT.fast
+--output ${rdir}/twas_sim${SIM}_loci${IDX}.fast
 
 echo "running std simulation"
 python /project/nmancuso_8/xwang505/twas_sim/sim.py \
-$oloci \
+${odir}/twas_sim_loci${IDX} \
 --ngwas $N \
 --nqtl $NGE \
 --ncausal $MODEL \
 --eqtl-h2 $H2G \
 --var-explained $H2GE \
 --linear-model $LINEAR_MODEL \
---output $OUT.std
+--output ${rdir}/twas_sim${SIM}_loci${IDX}.std
+
+#clean up!
+rm ${odir}/twas_sim_loci${IDX}*
