5648f3c6e8aec1bc9c8794661eef8b583107cfd9
xinranwang111 <88405078+xinranwang111@users.noreply.github.com>
2023-01-26 15:32:10 -0800

    Update example.slurm.sh

--- example.slurm.sh
+++ example.slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1
 #SBATCH --time=5:00:00
 #SBATCH --mem=8Gb
-#SBATCH --array=1-4
+#SBATCH --array=1-1
 
 if [ ! $SLURM_ARRAY_TASK_ID ]; then
   NR=$1
@@ -25,7 +25,8 @@ genes=glist-hg19.nodupe.autosome
 
 # PARAMETERS
 # !!! change to point to results/output directory !!!
-odir=/scratch1/xwang505/TWAS/
+odir=/project/nmancuso_8/xwang505/projects/twas_sim/output
+# /scratch1/xwang505/TWAS/
 
 # !!! change to point to plink installation !!!
 plink=/project/nmancuso_8/xwang505/tools/plink2
@@ -40,7 +41,7 @@ MIN_SNPS=450
 # PARAMETERS
 for IDX in `seq $start $stop`
 do
-  params=`sed "$((IDX+1))q;d" slurm.params`
+  params=`sed "$((IDX+1))q;d" param_twas_sim.tsv` #change back to slurm.params
   echo "$((IDX+1)) ${params}"
   set -- junk $params
   shift
@@ -51,11 +52,11 @@ do
   NGE=$3 # N EQTL
   MODEL=$4 # eQTL model; see sim.py for details
   H2G=$5 # eQTL h2g
-  H2GE=$6 # variance explained in complex trait; 0 (null) to 0.01 (huge effect) are reasonable values
+  H2GE=$6 # h2ge in complex trait; 0 (null) to 0.01 (huge effect) are reasonable values
   LINEAR_MODEL=$7
 
   # get genotype
-  while [[ ! -e $odir/twas_sim_loci${IDX}.bim ]]
+  while [[ ! -e ${odir}/twas_sim_loci${IDX}.bim ]]
   do
     echo "attempting ${IDX}"
     params=`shuf -n 1 $loci`
@@ -72,12 +73,12 @@ do
     # grab geno from reference panel
     # keep only common hapmap3 variants
     $plink \
-        --bfile $okg/1000G.EUR.QC.${numchr} \
+        --bfile ${okg}/1000G.EUR.QC.${numchr} \
         --chr $numchr \
         --from-bp $locus_start \
         --to-bp $locus_stop \
         --make-bed \
-        --out $odir/twas_sim_loci${IDX} \
+        --out ${odir}/twas_sim_loci${IDX} \
         --snps-only \
         --hwe midp 1e-5 \
         --geno 0.01 \
@@ -89,25 +90,39 @@ do
         --force-intersect
 
     # make sure we have at least MIN_SNPS in our data
-    if [[ `wc -l $odir/twas_sim_loci${IDX}.bim | awk '{print $1}'` -lt $MIN_SNPS ]];
+    if [[ `wc -l ${odir}/twas_sim_loci${IDX}.bim | awk '{print $1}'` -lt $MIN_SNPS ]];
     then
-      rm $odir/twas_sim_loci${IDX}.{bim,bed,fam}
+      rm ${odir}/twas_sim_loci${IDX}.{bim,bed,fam}
     fi
 
+    $plink \
+    --bfile ${odir}/twas_sim_loci${IDX} \
+    --keep EUR1.samples \
+    --make-bed \
+    --out ${odir}/twas_sim_sample1_loci${IDX}
+
+    $plink \
+    --bfile ${odir}/twas_sim_loci${IDX} \
+    --keep EUR2.samples \
+    --make-bed \
+    --out ${odir}/twas_sim_sample2_loci${IDX}
   done
 
   # run simulation
   echo "running fast simulation"
   python sim.py \
-      ${odir}/twas_sim_loci${IDX} \
+      ${odir}/twas_sim_sample1_loci${IDX} \
+      --eqtl-prefix ${odir}/twas_sim_sample2_loci${IDX} \
+      --test-prefix ${odir}/twas_sim_sample2_loci${IDX} \
       --ngwas $N \
       --nqtl $NGE \
       --ncausal $MODEL \
       --eqtl-h2 $H2G \
       --fast-gwas-sim \
-      --var-explained $H2GE \
+      --h2ge $H2GE \
       --linear-model $LINEAR_MODEL \
       --seed ${IDX} \
+      --genotype \
       --output ${odir}/twas_sim${SIM}_loci${IDX}
 done
 
