4317aa2662c0dbf4d8553c69c43ae8382579dcc8
quattro <nick.mancuso@gmail.com>
2023-03-08 11:14:33 -0800

    format code

--- external_py.py
+++ external_py.py
@@ -1,3 +1,23 @@
+"""
+This file is an example of how to define an external/custom function to fit a
+predictive model of gene expression from genotype to be used by `twas_sim`. Here
+we are using the `LinearRegression` class from `sklearn` to illustrate external
+python functionality.
+
+External modules -must- include a function named `fit` that takes as arguments:
+    Z: numpy matrix of genotype
+    y: numpy vector of gene expression/phenotype
+    h2g: the true h2g of gene expression
+    b_qtls: the true beta/effect-sizes for gene expression (i.e. eQTL)
+    args: the argparse object from twas_sim; useful for pulling `args.output`
+        as a prefix for temp data.
+
+Similarly, it must return a tuple containing (coef, r2, logl):
+    coef: the numpy vector for estimated eQTL weights
+    r2: the predictive r2 (optional; None)
+    logl: the log likelihood of the model (optional; None)
+
+"""
 import numpy as np
 from scipy import stats
 from sklearn import linear_model as lm
--- external_r.py
+++ external_r.py
@@ -1,5 +1,23 @@
-import subprocess
+"""
+This file is an example of how to define an external/custom function to fit a
+predictive model of gene expression from genotype to be used by `twas_sim`. Here
+we are illustrating how to call an external R script to call susieR on the simulated
+data.
 
+External modules -must- include a function named `fit` that takes as arguments:
+    Z: numpy matrix of genotype
+    y: numpy vector of gene expression/phenotype
+    h2g: the true h2g of gene expression
+    b_qtls: the true beta/effect-sizes for gene expression (i.e. eQTL)
+    args: the argparse object from twas_sim; useful for pulling `args.output`
+        as a prefix for temp data.
+
+Similarly, it must return a tuple containing (coef, r2, logl):
+    coef: the numpy vector for estimated eQTL weights
+    r2: the predictive r2 (optional; None)
+    logl: the log likelihood of the model (optional; None)
+
+"""
 import numpy as np
 
 
